c4eec63584e6ddabafae1956f0ab34c27ca29bb1
Imam-Ghazi-Khan <igkhan1710@gmail.com>
2023-12-26 20:06:04 +0530

    added mock data in case swiggy api fetch fails

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import RestaurantCard from "./Restaurant";
-import restaurantList, { SWIGGY_URL1, SWIGGY_URL2 } from "../utils/constants";
+import dataIfAPIFails, { SWIGGY_URL1, SWIGGY_URL2 } from "../utils/constants";
 import { useEffect, useState } from "react";
+import dataIfAPIFails from "../utils/constants";
 
 const Body = () => {
     const [listOfRestaurants,setListOfRestaurants] = useState([]);
@@ -9,11 +10,19 @@ const Body = () => {
    
    useEffect(()=>{
     const fetchData = async () => {
-        const data = await fetch(SWIGGY_URL2);
-        const json = await data.json();
-        const removedFirst2RestaurantCards = json.data.cards.slice(2);
-        setListOfRestaurants(removedFirst2RestaurantCards);
-        setCachedRestaurants(removedFirst2RestaurantCards);
+        try{
+            const data = await fetch(SWIGGY_URL2);
+            const json = await data.json();
+            //Optional Chaining
+            const removedFirst2RestaurantCards = json?.data?.cards?.slice(2);
+            setListOfRestaurants(removedFirst2RestaurantCards);
+            setCachedRestaurants(removedFirst2RestaurantCards);
+        }catch(error){
+            console.log("api fetch failed,hence loading mock data");
+            const dummyDataIfAPIFails = dataIfAPIFails?.data?.cards?.slice(2);
+            setListOfRestaurants(dummyDataIfAPIFails);
+            setCachedRestaurants(dummyDataIfAPIFails);
+        }
     }       
     fetchData();
    },[]);
@@ -21,26 +30,33 @@ const Body = () => {
     return (
         <div className="body">
             <div className="filter">
-                <input
-                    className="search-box"
-                    type="text" 
-                    value={searchBoxInput}
-                    placeholder="Search by name or cuisine.."
-                    onChange={e=>setSearchBoxInput(e.target.value)}
-                />
-                <button onClick={
-                    ()=>{
-                        let filteredList= cachedRestaurants.filter((restaurant) =>
-                        restaurant.card.card.info.name.toLowerCase().includes(searchBoxInput.toLowerCase()) ||
-                        restaurant.card.card.info.cuisines.join("").toLowerCase().includes(searchBoxInput.toLowerCase()));
-                        if(!searchBoxInput){
-                            setListOfRestaurants(cachedRestaurants);
+                
+                <div className="search">
+                    <input
+                        className="search-box"
+                        type="text" 
+                        value={searchBoxInput}
+                        placeholder="Search by name or cuisine.."
+                        onChange={e=>setSearchBoxInput(e.target.value)}
+                    />
+                    <button onClick={
+                        ()=>{
+                            if(!searchBoxInput){
+                                setListOfRestaurants(cachedRestaurants);
                             }
-                        else{
-                            setListOfRestaurants(filteredList);
+                            else{
+                                try{
+                                    let filteredList = cachedRestaurants.filter((restaurant) =>
+                                    restaurant.card.card.info.name.toLowerCase().includes(searchBoxInput.toLowerCase()) ||
+                                    restaurant.card.card.info.cuisines.join("").toLowerCase().includes(searchBoxInput.toLowerCase()));
+                                    setListOfRestaurants(filteredList);
+                                }catch(error){
+                                    console.log("please type something in search box");
+                                }
+                            }
                         }
-                    }
-                } className="search-btn">Search</button>
+                    } className="search-btn">Search</button>
+                </div>
 
                 <button onClick={
                     () => {                     
