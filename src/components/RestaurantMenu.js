9789ffb3895be25c798482f8177733f3bc9607fd
Imam-Ghazi-Khan <igkhan1710@gmail.com>
2024-01-01 17:58:26 +0530

    made restaurant-menu less ugly

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { SWIGGY_URL_MENU, itemCardsDataIfAPIFails, menuDataIfAPIFails } from '../utils/constants';
 import RestraurantCard from './RestaurantCard';
 import { useParams } from 'react-router-dom';
+import './RestaurantMenu.css'; 
 
 const RestaurantMenu = () => {
 
@@ -34,15 +35,21 @@ const RestaurantMenu = () => {
 
 
   return (
-    <div className='menu'>
-      <h1>{name}</h1>
-      <div>{costForTwoMessage+" - "+cuisines.join(", ")}</div>
+    <div className='menu-container'>
+      <div className='menu-header'>
+        <h1>{name}</h1>
+        <div className='restaurant-info'>{costForTwoMessage + " - " + cuisines.join(", ")}</div>
+      </div>
       <h2>Menu</h2>
-      <ul>
-        {itemCards.map((items,id) => <li key={id}>{items?.card?.info?.name}</li>)}
+      <ul className='menu-list'>
+        {itemCards.map((items, id) => (
+          <li key={id} className='menu-item'>
+            {items?.card?.info?.name}
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default RestaurantMenu
